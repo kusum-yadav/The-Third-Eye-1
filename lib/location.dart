94f638277285fb112bb2f4aa5950cd9374d0cd3d
mayankgupta1809 <mayankgupta1809@gmail.com>
2020-02-02 16:07:10 +0530

    added location

--- lib/location.dart
+++ lib/location.dart
@@ -3,13 +3,31 @@ import 'package:swipedetector/swipedetector.dart';
 import 'emergencylocation.dart';
 import 'package:flutter_tts/flutter_tts.dart';
 import 'package:vibration/vibration.dart';
+import 'package:location/location.dart';
+import 'package:flutter/services.dart';
+import 'dart:async';
 class location extends StatefulWidget {
   @override
   _locationState createState() => _locationState();
 }
-
 class _locationState extends State<location> {
   FlutterTts flutterTts=FlutterTts();
+  Map<String,double> currentLocation=new Map();
+  StreamSubscription<Map<String,double>> locationSubscription;
+  Location location=new Location();
+  void initState(){
+    super.initState();
+    currentLocation['latitude'] = 0.0;
+    currentLocation['longitude'] = 0.0;
+    
+    initPlatformState();
+    locationSubscription=location.onLocationChanged().listen((Map<String,double>result){
+      setState(() {
+        currentLocation=result;
+      });
+    }); 
+      }
+      String error;
   @override
   void vibrate()
   {
@@ -21,52 +39,73 @@ class _locationState extends State<location> {
   Future _speakright() async{
     await flutterTts.speak("emergency location sent to Harshit Gupta");
   }
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SwipeDetector( 
-        child: Container(
-          decoration: BoxDecoration(
-              image: DecorationImage(
-                  image: AssetImage("assets/images/location.jpg"),
-                  fit: BoxFit.cover,
+  Widget build(BuildContext context) {return Scaffold(
+          body:new Container(
+        child: SwipeDetector( 
+          child: Container(
+            alignment: Alignment.topCenter,
+            child: new Text('\n${currentLocation['latitude']}/${currentLocation['longitude']}',style: TextStyle(
+              fontSize: 70,
+              fontWeight: FontWeight.bold,
+            ),),
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage("assets/images/location.jpg"),
+                    fit: BoxFit.cover,
+            ),
           ),
-        ),
-        ),
-            // child: Image.asset('assets/images/Home.jpg'),
-            onSwipeUp: () {
-              
-              setState(() {
-                // _swipeDirection = "Swipe Up";
-              });
-            },
-            onSwipeDown: () {
-              setState(() {
-                // _swipeDirection = "Swipe Down";
-              });
-            },
-            onSwipeLeft: () {
-              vibrate();
-              _speakleft();
-              setState(() {
-                Navigator.pop(context);
-              });
-            },
-            onSwipeRight: () {
-              vibrate();
-              _speakright();
-              setState(() {
-                Navigator.push(context, MaterialPageRoute(builder: (context) =>emergencyLocation()));
-                // _swipeDirection = "Swipe Right";
-              });
-            },
-            swipeConfiguration: SwipeConfiguration(
-        verticalSwipeMinVelocity: 100.0,
-        verticalSwipeMinDisplacement: 50.0,
-        verticalSwipeMaxWidthThreshold:100.0,
-        horizontalSwipeMaxHeightThreshold: 50.0,
-        horizontalSwipeMinDisplacement:50.0,
-        horizontalSwipeMinVelocity: 200.0),
           ),
-        );
+              // child: Image.asset('assets/images/Home.jpg'),
+              onSwipeUp: () {
+                setState(() {
+                  // _swipeDirection = "Swipe Up";
+                });
+              },
+              onSwipeDown: () {
+                setState(() {
+                  // _swipeDirection = "Swipe Down";
+                });
+              },
+              onSwipeLeft: () {
+                vibrate();
+                _speakleft();
+                setState(() {
+                   Navigator.pop(context);
+                });
+              },
+              onSwipeRight: () {
+                vibrate();
+                _speakright();
+                setState(() {
+                  Navigator.push(context, MaterialPageRoute(builder: (context)=>emergencyLocation()));
+                  // _swipeDirection = "Swipe Right";
+                });
+              },
+              swipeConfiguration: SwipeConfiguration(
+          verticalSwipeMinVelocity: 100.0,
+          verticalSwipeMinDisplacement: 50.0,
+          verticalSwipeMaxWidthThreshold:100.0,
+          horizontalSwipeMaxHeightThreshold: 50.0,
+          horizontalSwipeMinDisplacement:50.0,
+          horizontalSwipeMinVelocity: 200.0),
+            ),
+          ),
+    );
   }
+  void initPlatformState() async {
+        Map<String,double> my_location;
+        try{
+          my_location=await location.getLocation();
+          error="";
+        }on PlatformException catch(e){
+          if(e.code == 'PERMISSION_DENIED')
+          error= 'Permission Denied';
+          else if(e.code== 'PERMISSION_DENIED_NEVER_ASK')
+          error= 'permission denied by user';
+          my_location=null;  
+        }
+        setState(() {
+          currentLocation=my_location;
+        });
+      }
 }
